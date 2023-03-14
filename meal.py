32759fe501b6f10c2bf57f1246675eba3ac682fd
Ahmadshahir <115374718+Ahmadshahir@users.noreply.github.com>
2023-03-14 19:06:50 -0400

    Add files via upload

---
+++ meal.py
@@ -0,0 +1,8 @@
+class Meal:
+    def __init__(self, name, calories, nutrients, recipe):
+        self.name = name
+        self.calories = calories
+        self.nutrients = nutrients
+        self.recipe = recipe
+
+
